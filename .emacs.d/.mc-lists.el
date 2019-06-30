13abafb413bca89d8717222aaad00247fd1176b7
Andrew Hyatt <ahyatt@gmail.com>
2019-06-30 16:44:08 -0400

    Hydra-helm config added.

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -9,11 +9,13 @@
 (setq mc/cmds-to-run-once
       '(
         helm-M-x
-        mc/mark-next-lines
-        hydra-multiple-cursors/mc/mark-next-like-this
-        hydra-multiple-cursors/mc/mark-all-like-this-and-exit
+        hydra-multiple-cursors/mc/insert-letters-and-exit
         hydra-multiple-cursors/mc/mark-all-dwim-and-exit
+        hydra-multiple-cursors/mc/mark-all-like-this-and-exit
         hydra-multiple-cursors/mc/mark-next-like-this
-        hydra-multiple-cursors/mc/mark-previous-like-this
+        hydra-multiple-cursors/mc/mark-next-like-this
+        hydra-multiple-cursors/mc/mark-next-lines
         hydra-multiple-cursors/mc/mark-next-lines-and-exit
+        hydra-multiple-cursors/mc/mark-previous-like-this
+        mc/mark-next-lines
         ))
